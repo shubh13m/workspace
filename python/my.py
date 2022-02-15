428fd29faed35ae7eb6655983d2f6ac3befd727c
shubh13m <shubhpan1996@gmail.com>
2022-02-16 03:03:21 +0530

    Finished Python Automation Book Tutorial.

--- python/my.py
+++ python/my.py
@@ -1,16 +1,19 @@
+import sys
 import re
 import os
-import numpy as np
-import pandas as pnd
+import webbrowser
+# import numpy as np
+# import pandas as pnd
 
-arr = np.array([1, 2, 3])
-print("Array with Rank 1: \n",arr)
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
-df = pnd.DataFrame(data)
-print(df)
+# arr = np.array([1, 2, 3])
+# print("Array with Rank 1: \n",arr)
+# data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+#         'Age':[20, 21, 19, 18]}
+# df = pnd.DataFrame(data)
+# print(df)
 print("shubham pandey")
 
-name = input("Enter first name")
-date = input("Enter birth date")
-print(name+date)
+# name = input("Enter first name")
+# date = input("Enter birth date")
+# print(name+date)
+webbrowser.open(sys.argv[1:][0])
